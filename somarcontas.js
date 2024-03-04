9e74f871c05e29e7a43c0559e8b5fe7d12b3bde8
Rafael Matos <rafaelmattos001@gmail.com>
2024-03-04 00:08:52 -0300

    teste jquery

--- somarcontas.js
+++ somarcontas.js
@@ -1,24 +1,2 @@
-var botaoSomar = document.querySelector("#somar")
-soma = 0;
-botaoSomar.addEventListener("click", somarDespesasF);
-
-
-
-function somarDespesasF() {
-    var contasF = document.querySelectorAll(".contasf");
-    var soma = 0;
-  
-    for (var i = 0; i < contasF.length; i++) {
-      var valor = parseFloat(contasF[i].value);
-      if (!isNaN(valor)) {
-        soma += valor;
-      } 
-    } 
-
-    console.log("Valor da soma:", soma);
-
-  
-   var totalSoma = document.querySelector("#itotall");
-   totalSoma.textContent = soma;
-  }
+$("h1").css("color", "black");
   
