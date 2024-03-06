4ad8c5b434a7892c8b7c771adfa31ed981c8eae1
Rafael Matos <rafaelmattos001@gmail.com>
2024-03-06 00:53:29 -0300

    enviando divisões por pessoas

--- somar.js
+++ somar.js
@@ -1,26 +1,48 @@
-var botao = document.querySelector("#calcular")
-botao.addEventListener("click", calculacontas)
 
 
-function calculacontas(){
+var botao = document.querySelector("#calcular");
+botao.addEventListener("click", calculacontas);
 
-    var contasfixas = document.querySelectorAll(".contasf")
 
+function calculacontas() {
+    var contasfixas = document.querySelectorAll(".contasf");
     var total = 0;
 
     contasfixas.forEach(function(input) {
-
         if (input.value.trim() !== "") {
-            
             total += parseFloat(input.value);
         }
-       
-        
     });
 
-    var resultado = document.querySelector("#total")
-    resultado.textContent = total
+    var resultado = document.querySelector("#total");
+    resultado.textContent = total;
 }
+
+$("#pessoa").click(function() {
+    var total = parseFloat($('#total').text());
+    var novoTotal = total / 1;
+    $('#total').text(novoTotal);
+});
+
+$("#pessoas").click(function() {
+    var total = parseFloat($('#total').text());
+    var novoTotal = total / 2;
+    $('#total').text(novoTotal);
+});
+
+$("#pessoas1").click(function() {
+    var total = parseFloat($('#total').text());
+    var novoTotal = total / 3;
+    $('#total').text(novoTotal);
+});
+
+$("#pessoas2").click(function() {
+    var total = parseFloat($('#total').text());
+    var novoTotal = total / 2;
+    $('#total').text(novoTotal);
+});
+
+
 
 
 
