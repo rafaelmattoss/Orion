33cb82495571fdc5b1ff2bff1f5eeb2f59134aea
Rafael Matos <rafaelmattos001@gmail.com>
2024-03-10 18:54:43 -0300

    atts no menu

--- funcoes.js
+++ funcoes.js
@@ -1,10 +1,28 @@
 
+let menuFToggle = false;
+let menuVToggle = false
+
 $("#menuf").click(() => {
     $("#todasfixas").toggle();
+    $("#calcular").toggle();
+    menuFToggle = !menuFToggle;
+    if (menuFToggle) {
+        $("#menuf").text("X");
+    } else {
+        $("#menuf").text("Despesas Fixas");
+    }
 });
 
 $("#menuv").click(() => {
-    $(" #todasvariaveis").toggle();
+    $("#todasvariaveis").toggle();
+    $("#calcular").toggle();
+
+    menuVToggle = !menuVToggle;
+    if (menuVToggle) {
+        $("#menuv").text("X");
+    } else {
+        $("#menuv").text(" Despesas Variaveis");
+    }
 });
 
 $("#menuf, #menuv").click(() => {
