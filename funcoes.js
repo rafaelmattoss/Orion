3c53c379459ed1bc89011db89fd06c2cebe8ae5c
Rafael Matos <rafaelmattos001@gmail.com>
2024-03-12 02:19:32 -0300

    muitas novas atts
    inclusive no disgner

--- funcoes.js
+++ funcoes.js
@@ -1,44 +1,36 @@
-
 let menuFToggle = false;
-let menuVToggle = false
+let menuVToggle = false;
 
 $("#menuf").click(() => {
     $("#todasfixas").toggle();
     menuFToggle = !menuFToggle;
     if (menuFToggle) {
         $("#menuf").text("X");
+        $("#menuf").addClass("fechar"); 
+        $("#calcular").show(); 
+        $("#total").removeClass('ocultar'); 
     } else {
         $("#menuf").text("Despesas Fixas");
+        $("#menuf").removeClass("fechar");
+        $("#total").addClass('ocultar'); 
+        $("#calcular").hide(); 
+        $("#total").ClassAdd('ocultar');
     }
 });
 
 $("#menuv").click(() => {
     $("#todasvariaveis").toggle();
-
     menuVToggle = !menuVToggle;
     if (menuVToggle) {
         $("#menuv").text("X");
+        $("#menuv").addClass("fechar"); 
+        $("#calcular").show(); 
+        $("#total").removeClass('ocultar');
     } else {
-        $("#menuv").text(" Despesas Variaveis");
+        $("#menuv").text("Despesas Variáveis");
+        $("#menuv").removeClass("fechar");
+        $("#total").addClass('ocultar');
+        $("#calcular").hide(); 
+        
     }
 });
-
-$("#menuf, #menuv").click(() => {
-
-    $("#calcular").toggle();
-});
-
-
-
-
-
-
-
-
-
-
-  
-  
-  
-
-
