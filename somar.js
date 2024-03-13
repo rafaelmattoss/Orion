bf356e80f2e28496d9b014f2a8d66e4d169d02e3
Rafael Matos <rafaelmattos001@gmail.com>
2024-03-13 02:23:22 -0300

    novas atts

--- somar.js
+++ somar.js
@@ -1,9 +1,9 @@
-
 var resultadosDivisao = [];
-
+var salarioOriginal;
 
 $("#calcular").click(function() {
-    var salario = $( "#salario" ).val(); 
+    var salario = parseFloat($("#salario").val()); 
+    salarioOriginal = salario; 
     var contasfixas = document.querySelectorAll(".contasf");
     var contasVariaveis = document.querySelectorAll(".contasv");
     var total = 0;
@@ -20,35 +20,50 @@ $("#calcular").click(function() {
         }
     });
 
-    
     $('#total').text(total.toFixed(2) + "R$");
 
     divisao(total);
+});
 
-    var totalreal = (salario - total)
+$("#total").click(() => {
+    var salario = parseFloat($("#salario").val());
+    var totalAtual = parseFloat($('#total').text());
+    var novoSalario = salario - totalAtual;
+    $("#salario").val(novoSalario.toFixed(2));
 
-    $("#salario").val(totalreal);
-
+    $("#total").addClass("pulsate");
+    
+    // Remove a classe pulsate após 1 segundo
+    setTimeout(() => {
+        $("#total").removeClass("pulsate");
+    }, 1000);
 });
 
 
 function divisao(totalOriginal) {
     resultadosDivisao = [];
-
     resultadosDivisao.push(totalOriginal / 2);
     resultadosDivisao.push(totalOriginal / 3);
     resultadosDivisao.push(totalOriginal / 4);
 }
 
-
 $("#pessoas").click(function() {
-    $('#total').text(resultadosDivisao[0].toFixed(2) + 'R$'); 
+    var valorDivisao = resultadosDivisao[0];
+    var novoSalario = salarioOriginal - valorDivisao; 
+    $('#total').text(valorDivisao.toFixed(2) + 'R$');
+    $("#salario").val(novoSalario.toFixed(2)); 
 });
 
 $("#pessoas1").click(function() {
-    $('#total').text(resultadosDivisao[1].toFixed(2) + 'R$');
+    var valorDivisao = resultadosDivisao[1];
+    var novoSalario = salarioOriginal - valorDivisao; 
+    $('#total').text(valorDivisao.toFixed(2) + 'R$');
+    $("#salario").val(novoSalario.toFixed(2)); 
 });
 
 $("#pessoas2").click(function() {
-    $('#total').text(resultadosDivisao[2].toFixed(2) + 'R$');
+    var valorDivisao = resultadosDivisao[2];
+    var novoSalario = salarioOriginal - valorDivisao; 
+    $('#total').text(valorDivisao.toFixed(2) + 'R$');
+    $("#salario").val(novoSalario.toFixed(2)); 
 });
