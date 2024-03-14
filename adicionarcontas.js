c7f2a0e575e23a4cf705c4224e279a2b8c540c8c
Rafael Matos <rafaelmattos001@gmail.com>
2024-03-14 20:04:08 -0300

    concertei uns bugs

--- adicionarcontas.js
+++ adicionarcontas.js
@@ -1,28 +1,57 @@
-$(document).ready(function() {
-    $("#addfixa").click(function() {
-        var descricao = $("#descricao").val();
-        var valor = $("#valor").val();
+$(document).ready(()=>{
+   $("#addfixa").click(()=>{
 
-       
-        if (descricao !== "" && valor !== "") {
-            
-            var novaDespesa = $("<div ></div>").addClass("df")
-            var novolabel = $("<label></label>").addClass("lf")
-            var novoInput = $("<input>").addClass("contasf");
+    var descricao = $("#descricao").val();
+    var valor = $("#valor").val();
 
-            novolabel.text(descricao)
-            novoInput.val(valor);
-            novaDespesa.append(novolabel, novoInput)
+   
+    if (descricao !== "" && valor !== "") {
+        
+        var novaDespesa = $("<div></div>").addClass("df");
+        var novolabel = $("<label></label>").addClass("lf")
+        var novoInput = $("<input>").addClass("contasf");
 
-            $("#todasfixas").append(novaDespesa);
+        novolabel.text(descricao)
+        novoInput.val(valor);
+        novaDespesa.append(novolabel, novoInput)
 
-            $("#descricao").val("");
-            $("#valor").val("");
-        } else {
-            alert("Por favor, preencha ambos os campos.");
-        }
-    });
+        $("#todasfixas").append(novaDespesa);
 
+        $("#descricao").val("");
+        $("#valor").val("");
+    } else {
+        alert("Por favor, preencha ambos os campos.");
+    }
+
+   })
+
+   $("#addvariavel").click(()=>{
 
     
-});
+    var despesav = $("#descricaovv").val();
+    var valorv = $("#valorv").val();
+
+    if(despesav !== "" &&  valorv !==""){
+
+    var novaDespesav=   $("<div></div>").addClass("dv");
+    var novolabelv = $("<label></label>").addClass("lv"); 
+    var novoinputv = $("<input>").addClass("contasv")
+
+    novolabelv.text(despesav)
+    novoinputv.val(valorv)
+
+    novaDespesav.append(novolabelv, novoinputv);
+
+    $("#todasvariaveis").append(novaDespesav)
+
+    $("despesav").val("");
+    $  ("#valorv").val("");
+    } else{
+        alert(" os dois campos precisam ser preenchidos")
+    }
+
+
+   })
+       
+})
+    
