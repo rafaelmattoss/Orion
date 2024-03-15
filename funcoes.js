1458c2bec4a98f3715083bcd28bb3d9c9556d574
Rafael Matos <rafaelmattos001@gmail.com>
2024-03-15 00:36:16 -0300

    funções

--- funcoes.js
+++ funcoes.js
@@ -4,43 +4,55 @@ let menuVToggle = false;
 $("#menuf").click(() => {
     $("#todasfixas").toggle();
     menuFToggle = !menuFToggle;
+
     $("#df").toggle();
 
-    if (menuFToggle) {
-        $("#menuf").text("X");
-        $("#menuf").addClass("fechar"); 
+    if (menuFToggle || (menuVToggle && !menuFToggle)) {
         $("#calcular").show(); 
-        $("#total").removeClass('ocultar'); 
+        $("#total").removeClass('ocultar');
     } else {
-        $("#menuf").text("Despesas Fixas");
-        $("#menuf").removeClass("fechar");
-        $("#total").addClass('ocultar'); 
+        $("#total").addClass('ocultar');
         $("#calcular").hide(); 
         $("#descricaof").hide();
         $("#descricaov").hide();
-        
     }
+
+    if (menuFToggle) {
+        $("#menuf").text("X");
+        $("#menuf").addClass("fechar");
+    } else{
+        $("#menuf").text("Despesas Fixas");
+        $("#menuf").removeClass("fechar");
+    }
+
 });
+
+
 
 
 $("#menuv").click(() => {
     $("#todasvariaveis").toggle();
     menuVToggle = !menuVToggle;
-    if (menuVToggle) {
-        $("#menuv").text("X");
-        $("#menuv").addClass("fechar"); 
+
+    if (menuVToggle || (menuFToggle && !menuVToggle)) {
         $("#calcular").show(); 
         $("#total").removeClass('ocultar');
     } else {
-        $("#menuv").text("Despesas Variáveis");
-        $("#menuv").removeClass("fechar");
         $("#total").addClass('ocultar');
         $("#calcular").hide(); 
         $("#descricaof").hide();
         $("#descricaov").hide();
+    }
 
+    if (menuVToggle) {
+        $("#menuv").text("X");
+        $("#menuv").addClass("fechar");
+    } else{
+        $("#menuv").text("Despesas Variáveis");
+        $("#menuv").removeClass("fechar");
     }
 });
+    
 
 
 
@@ -51,4 +63,4 @@ $("#adcontasf").click(()=>{
 
 $("#adcontasv").click(()=>{
     $("#descricaov").toggle();
-})
+});
